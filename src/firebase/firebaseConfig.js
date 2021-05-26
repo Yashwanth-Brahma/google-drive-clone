1fd427ea877e51a62ac33e3498437159890f4ff5
Yash <yash1997bm@gmail.com>
2021-05-27 04:14:44 +0530

    firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBhcyiqR3sylrhsbgdR45Jk9aoeXf9opRU",
+  authDomain: "drive-clone-d3926.firebaseapp.com",
+  projectId: "drive-clone-d3926",
+  storageBucket: "drive-clone-d3926.appspot.com",
+  messagingSenderId: "999629823349",
+  appId: "1:999629823349:web:e61515f690bc5fd193473c",
 });
 
 export const auth = app.auth();
@@ -24,3 +24,10 @@ export const database = {
   currentTimeStamp: firebase.firestore.FieldValue.serverTimestamp,
 };
 export default app;
+
+// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+// projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+// storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_FIREBASE_APP_ID,
